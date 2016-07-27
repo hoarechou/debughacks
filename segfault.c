2974bbfeae431a0d51da13577aedb41cfb990070
house.chou <house.chou@delta.com>
2016-07-27 11:16:51 +0800

    first commit

---
+++ segfault.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int *a =NULL;
+
+	*a = 0x1;
+
+	
+	return 0;
+}
---
+++ watchdemo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void foo(){
+
+	int i = 0;
+	
+	for(i=11;i<10;i++){
+
+
+	}
+
+
+}
+
+
+
+int main(){
+
+	int i = 0;
+
+	for (i=0;i<10;i++){
+		printf("");
+	}
+	
+
+	foo();
+
+	return 0;
+
+}
